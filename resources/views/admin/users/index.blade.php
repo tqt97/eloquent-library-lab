84b420c1b80d51f51cb7646bafc8107bb3933299
tqt97 <kutuanonline199@gmail.com>
2025-11-13 21:23:35 +0700

    Improve display last_login_at in view

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -32,7 +32,8 @@
                                                     <x-table.td value="{{ $user->first_name }}" />
                                                     <x-table.td value="{{ $user->last_name }}" />
                                                     <x-table.td value="{{ $user->email }}" />
-                                                    <x-table.td value="{{ $user->last_login_at?->diffForHumans() }}" />
+                                                    <x-table.td
+                                                        value="{{ $user->last_login_at ? $user->last_login_at->diffForHumans() : '-' }}" />
                                                     <x-table.td class="text-right">
                                                         <a href="" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                                     </x-table.td>
